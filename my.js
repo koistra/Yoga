0729e59eb083383f677b49304bb69b63936627a8
koistra <33932558+koistra@users.noreply.github.com>
2018-07-24 14:22:20 +0300

    new verion

---
+++ my.js
@@ -0,0 +1,9 @@
+	function openNav() {
+    document.getElementById("mySidenav").style.width = "260px";
+    document.getElementById("main").style.marginLeft = "260px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
